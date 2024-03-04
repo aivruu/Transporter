7e6dfa5a1551b63ff2df5fe487720f7af50dc334
Qekly <benjaalejandro206@gmail.com>
2024-03-04 20:35:06 -0300

    feat: Define global settings for project on main build script

---
+++ build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    java
+    alias(libs.plugins.indra)
+    alias(libs.plugins.indra.checkstyle)
+    alias(libs.plugins.spotless)
+}
+
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "net.kyori.indra")
+    apply(plugin = "net.kyori.indra.checkstyle")
+    apply(plugin = "com.diffplug.spotless")
+
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        mavenLocal()
+
+        maven("https://repo.papermc.io/repository/maven-public/")
+        maven("https://repo.triumphteam.dev/snapshots/")
+        maven("https://repo.unnamed.team/repository/unnamed-public/")
+        maven("https://repo.alessiodp.com/releases/")
+    }
+    
+    indra {
+        javaVersions {
+            target(17)
+            minimumToolchain(17)
+        }
+        checkstyle("10.13.0")
+    }
+
+    spotless {
+        java {
+            licenseHeaderFile("$rootDir/header.txt")
+            removeUnusedImports()
+            trimTrailingWhitespace()
+            indentWithSpaces(2)
+        }
+        kotlinGradle {
+            trimTrailingWhitespace()
+            indentWithSpaces(2)
+        }
+    }
+
+    dependencies {
+        compileOnly("team.unnamed:inject:2.0.1") // Include Inject library to the subprojects
+        checkstyle("ca.stellardrift:stylecheck:0.2.1")
+    }
+
+    tasks {
+        compileJava {
+            dependsOn("spotlessApply")
+            dependsOn("checkstyleMain")
+            options.compilerArgs.add("-parameters")
+        }
+    }
+}
