0ea7647c25a7d995ce343117c555b685e63c9745
Qekly <benjaalejandro206@gmail.com>
2024-03-06 22:57:12 -0300

    feat: Include adventure-text-minimessage dependency

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -3,13 +3,15 @@ plugins {
 }
 
 publishing {
-	  publications {
-	      create<MavenPublication>("maven") {
-	          from(components["java"])
-	      }
-	  }
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
 }
 
 dependencies {
+    compileOnly("org.jetbrains:annotations:24.0.1")
+    compileOnly(libs.minimessage)
     compileOnly(libs.configurate)
 }
